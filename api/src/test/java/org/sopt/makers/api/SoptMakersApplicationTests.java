f780142985aee86228ff9af9996480f9b8d8eccb
sung-silver <jinna0319@gmail.com>
2026-05-31 15:18:11 +0900

    chore: 빌드 테스트를 위한 resources 및 mock 추가

--- api/src/test/java/org/sopt/makers/api/SoptMakersApplicationTests.java
+++ api/src/test/java/org/sopt/makers/api/SoptMakersApplicationTests.java
@@ -1,10 +1,18 @@
 package org.sopt.makers.api;
 
 import org.junit.jupiter.api.Test;
+import org.sopt.makers.domain.auth.port.OAuthAuthenticatorPort;
+import org.sopt.makers.domain.auth.port.SmsSenderPort;
+import org.sopt.makers.domain.auth.port.TokenIssuerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class SoptMakersApplicationTests {
+
+  @MockitoBean OAuthAuthenticatorPort oAuthAuthenticatorPort;
+  @MockitoBean TokenIssuerPort tokenIssuerPort;
+  @MockitoBean SmsSenderPort smsSenderPort;
 
   @Test
   void contextLoads() {}
