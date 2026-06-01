4524d14bf9037d9f2f866a9638003a0bdb465843
sung-silver <jinna0319@gmail.com>
2026-06-01 22:32:22 +0900

    add: jwt 관련 의존성 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
