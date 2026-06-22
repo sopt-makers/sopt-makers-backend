94a67eba93a67d7845e482ebf6d656e44654bcab
dlwjddus1112 <dlwjddus1112@naver.com>
2026-06-22 20:31:40 +0900

    feat : 활둥 후기 관련 예외 클래스 추가

---
+++ domain/domain-official/src/main/java/org/sopt/makers/domain/official/homepage/review/exception/HomepageReviewFailure.java
@@ -0,0 +1,21 @@
+package org.sopt.makers.domain.official.homepage.review.exception;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.core.code.FailureCode;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public enum HomepageReviewFailure implements FailureCode {
+  NOT_FOUND_HOMEPAGE_REVIEW(404, "존재하지 않는 홈페이지 리뷰입니다");
+
+  private final int statusCode;
+  private final String message;
+
+  @Override
+  public int getStatusCode() {
+    return statusCode;
+  }
+}
