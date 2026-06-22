f7158d8447d681515cf490d4696430e6f64f4dda
dlwjddus1112 <dlwjddus1112@naver.com>
2026-06-22 20:33:41 +0900

    feat : 활동 후기 관련 API 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation(project(":domain:domain-playground"))
     implementation(project(":domain:domain-app"))
     implementation(project(":domain:domain-admin"))
+    implementation(project(":domain:domain-official"))
     implementation(project(":storage"))
     implementation(project(":clients"))
 
