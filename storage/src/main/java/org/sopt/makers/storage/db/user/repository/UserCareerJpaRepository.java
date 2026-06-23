883ed0ba5d7193dcbf3a2a1655c1c6039a826956
sung-silver <jinna0319@gmail.com>
2026-06-24 07:50:22 +0900

    refactor: career, link 관련 n+1 문제 개선

--- storage/src/main/java/org/sopt/makers/storage/db/user/repository/UserCareerJpaRepository.java
+++ storage/src/main/java/org/sopt/makers/storage/db/user/repository/UserCareerJpaRepository.java
@@ -2,8 +2,13 @@ package org.sopt.makers.storage.db.user.repository;
 
 import org.sopt.makers.storage.db.user.entity.UserCareerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserCareerJpaRepository extends JpaRepository<UserCareerEntity, Long> {
 
-  void deleteAllByUserId(Long userId);
+  @Modifying
+  @Query("delete from UserCareerEntity c where c.userId = :userId")
+  void deleteAllByUserId(@Param("userId") Long userId);
 }
--- storage/src/main/java/org/sopt/makers/storage/db/user/repository/UserLinkJpaRepository.java
+++ storage/src/main/java/org/sopt/makers/storage/db/user/repository/UserLinkJpaRepository.java
@@ -2,8 +2,13 @@ package org.sopt.makers.storage.db.user.repository;
 
 import org.sopt.makers.storage.db.user.entity.UserLinkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserLinkJpaRepository extends JpaRepository<UserLinkEntity, Long> {
 
-  void deleteAllByUserId(Long userId);
+  @Modifying
+  @Query("delete from UserLinkEntity l where l.userId = :userId")
+  void deleteAllByUserId(@Param("userId") Long userId);
 }
