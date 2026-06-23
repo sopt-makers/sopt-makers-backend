c369965cacc849aff453646a078cb5b27ab44c44
dlwjddus1112 <dlwjddus1112@naver.com>
2026-06-23 19:32:08 +0900

    feat : News 도메인 모델 구현

---
+++ domain/domain-official/src/main/java/org/sopt/makers/domain/official/homepage/news/News.java
@@ -0,0 +1,20 @@
+package org.sopt.makers.domain.official.homepage.news;
+
+import java.time.LocalDateTime;
+
+public record News(
+    Integer id,
+    String imageUrl,
+    String title,
+    String link,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt) {
+
+  public static News create(String imageUrl, String title, String link) {
+    return new News(null, imageUrl, title, link, null, null);
+  }
+
+  public News update(String imageUrl, String title, String link) {
+    return new News(id, imageUrl, title, link, createdAt, updatedAt);
+  }
+}
