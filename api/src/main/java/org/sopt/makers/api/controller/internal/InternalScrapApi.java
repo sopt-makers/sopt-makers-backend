5cd7226f507770008f35a2343c44e1bbaff48a32
dietken1 <dietken1@ajou.ac.kr>
2026-06-27 02:01:02 +0900

    fix: InternalScrapApi 인터페이스에 @Valid 추가

--- api/src/main/java/org/sopt/makers/api/controller/internal/InternalScrapApi.java
+++ api/src/main/java/org/sopt/makers/api/controller/internal/InternalScrapApi.java
@@ -2,6 +2,7 @@ package org.sopt.makers.api.controller.internal;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.sopt.makers.api.controller.internal.dto.InternalScrapRequest;
 import org.sopt.makers.core.response.BaseResponse;
 import org.springframework.http.ResponseEntity;
@@ -10,5 +11,5 @@ import org.springframework.http.ResponseEntity;
 public interface InternalScrapApi {
 
   @Operation(summary = "블로그 링크 스크랩")
-  ResponseEntity<BaseResponse<?>> scrap(InternalScrapRequest.Scrap request);
+  ResponseEntity<BaseResponse<?>> scrap(@Valid InternalScrapRequest.Scrap request);
 }
