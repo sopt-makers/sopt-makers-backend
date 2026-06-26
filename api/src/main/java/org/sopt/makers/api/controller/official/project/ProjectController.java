f7944f1f275c6976b4d47548c1be5cdb03392f64
dietken1 <dietken1@ajou.ac.kr>
2026-06-27 02:14:11 +0900

    fix: ProjectController @Validated 추가

--- api/src/main/java/org/sopt/makers/api/controller/official/project/ProjectController.java
+++ api/src/main/java/org/sopt/makers/api/controller/official/project/ProjectController.java
@@ -16,11 +16,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/projects")
 @RequiredArgsConstructor
+@Validated
 public class ProjectController implements ProjectApi {
 
   private final ProjectService projectService;
