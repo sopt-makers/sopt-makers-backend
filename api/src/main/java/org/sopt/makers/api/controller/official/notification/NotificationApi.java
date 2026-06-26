d6308b22345e223073ab0e93c997179b1a33fa99
sung-silver <jinna0319@gmail.com>
2026-06-26 23:31:36 +0900

    refactor: NotificationController를 NotificationApi/NotificationController로 분리

---
+++ api/src/main/java/org/sopt/makers/api/controller/official/notification/NotificationApi.java
@@ -0,0 +1,17 @@
+package org.sopt.makers.api.controller.official.notification;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.sopt.makers.api.controller.official.notification.dto.NotificationRequest;
+import org.sopt.makers.core.response.BaseResponse;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "알림 신청", description = "공식 홈페이지 알림 신청 API")
+public interface NotificationApi {
+
+  @Operation(summary = "알림 신청")
+  ResponseEntity<BaseResponse<?>> register(NotificationRequest.Register request);
+
+  @Operation(summary = "알림 신청 목록 조회")
+  ResponseEntity<BaseResponse<?>> getList(Integer generation);
+}
