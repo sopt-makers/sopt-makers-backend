2e04f780a547f8382b0e2b35c42f306d2f4a5b1e
sung-silver <jinna0319@gmail.com>
2026-06-26 23:04:46 +0900

    chore: 불필요한 설정 주석 제거

--- api/src/main/java/org/sopt/makers/api/common/config/ApplicationConfig.java
+++ api/src/main/java/org/sopt/makers/api/common/config/ApplicationConfig.java
@@ -9,7 +9,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties({
   AuthProperty.class,
   SecurityProperty.class,
-  // ApplicationProperty.class  → storage 모듈 구현 시 storage.config로 이동
-  // ExternalProperty.class     → clients 모듈 구현 시 clients.config로 이동
 })
 public class ApplicationConfig {}
