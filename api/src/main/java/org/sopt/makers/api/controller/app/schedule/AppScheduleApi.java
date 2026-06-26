1833a68c5cf61a583f7d8dbbfc5653b1c03e6675
sung-silver <jinna0319@gmail.com>
2026-06-27 01:27:27 +0900

    feat: 앱 일정 조회 api swagger 설명 추가

--- api/src/main/java/org/sopt/makers/api/controller/app/schedule/AppScheduleApi.java
+++ api/src/main/java/org/sopt/makers/api/controller/app/schedule/AppScheduleApi.java
@@ -1,6 +1,7 @@
 package org.sopt.makers.api.controller.app.schedule;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDateTime;
 import org.sopt.makers.core.response.BaseResponse;
@@ -10,5 +11,7 @@ import org.springframework.http.ResponseEntity;
 public interface AppScheduleApi {
 
   @Operation(summary = "일정 리스트 조회")
-  ResponseEntity<BaseResponse<?>> getSchedules(LocalDateTime start, LocalDateTime end);
+  ResponseEntity<BaseResponse<?>> getSchedules(
+      @Parameter(description = "조회 시작 일시") LocalDateTime start,
+      @Parameter(description = "조회 종료 일시") LocalDateTime end);
 }
