752c04d0e32ab53859b484433400f254dc0cb0ab
dlwjddus1112 <dlwjddus1112@naver.com>
2026-06-28 16:54:20 +0900

    feat : Generation 도메인 구현

---
+++ domain/domain-official/src/main/java/org/sopt/makers/domain/official/generation/BrandingColor.java
@@ -0,0 +1,7 @@
+package org.sopt.makers.domain.official.generation;
+
+public record BrandingColor(
+    String darkModeKeyColor,
+    String darkModeTextColor,
+    String lightModeKeyColor,
+    String lightModeTextColor) {}
