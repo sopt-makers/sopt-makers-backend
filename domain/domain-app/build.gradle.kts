911f3b04fdb360fac44948b2afec52043885ec54
jher235 <tim668666@gmail.com>
2026-07-16 21:18:17 +0900

    test: fortune 운세 갱신 로직 테스트 작성

--- domain/domain-app/build.gradle.kts
+++ domain/domain-app/build.gradle.kts
@@ -3,4 +3,8 @@ dependencies {
     implementation(project(":domain:domain-user"))
     implementation("org.springframework:spring-context")
     implementation("org.springframework:spring-tx")
+
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
