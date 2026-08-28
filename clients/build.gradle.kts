9b0e9d8d60507035e5d3159792cef482b6f9b2f6
Yerin Lee <rt8632@naver.com>
2026-08-28 13:44:26 +0900

    fix : conflict 해결

--- clients/build.gradle.kts
+++ clients/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation(project(":domain:domain-official"))
     implementation(project(":domain:domain-playground"))
     implementation(project(":domain:domain-app"))
+    implementation(project(":domain:domain-playground"))
 
 
     implementation("org.springframework.boot:spring-boot-starter")
