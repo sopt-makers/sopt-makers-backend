bc2fd1252aa0e91ec2686f32d2afea76c71592d0
hyerinhwang-sailin <sallyh1111@ewhain.net>
2026-09-05 05:11:04 +0900

    feat(community): migrate text and web link utilities

---
+++ domain/domain-playground/src/main/java/org/sopt/makers/domain/playground/community/utils/MentionCleaner.java
@@ -0,0 +1,20 @@
+package org.sopt.makers.domain.playground.community.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MentionCleaner {
+    private static final Pattern mentionPattern = Pattern.compile("@(.*?)\\[\\d+\\]");
+
+    public static String removeMentionIds(String content) {
+        Matcher matcher = mentionPattern.matcher(content);
+        StringBuilder sb = new StringBuilder();
+
+        while (matcher.find()) {
+            String nickname = matcher.group(1);
+            matcher.appendReplacement(sb, "@" + nickname);
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+}
