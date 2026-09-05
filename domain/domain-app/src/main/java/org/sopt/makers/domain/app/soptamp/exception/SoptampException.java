9f71fc80ed4e316402d6005bb11ddcbd5b7aeeaa
jher235 <tim668666@gmail.com>
2026-09-05 17:32:31 +0900

    feat: 솝탬프 에러코드 추가

--- domain/domain-app/src/main/java/org/sopt/makers/domain/app/soptamp/exception/SoptampException.java
+++ domain/domain-app/src/main/java/org/sopt/makers/domain/app/soptamp/exception/SoptampException.java
@@ -3,7 +3,6 @@ package org.sopt.makers.domain.app.soptamp.exception;
 import org.sopt.makers.core.exception.BaseException;
 
 public class SoptampException extends BaseException {
-
   public SoptampException(SoptampFailure failure) {
     super(failure);
   }
